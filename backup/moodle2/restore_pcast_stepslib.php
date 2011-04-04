eaa5c1b59dd67eae8b712b71567b3325e4feb64b
sbourget <sbourget@854d0719-7c51-1d46-9240-c4c6275e8ce8>
2011-04-04 19:56:40 +0000

    First cut at restore scripts (File handling is broken)
    git-svn-id: http://ghs-svn.sau19.org/svn/MDL_Mod_Pcast/trunk@122 854d0719-7c51-1d46-9240-c4c6275e8ce8

---
+++ backup/moodle2/restore_pcast_stepslib.php
@@ -0,0 +1,101 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package moodlecore
+ * @subpackage backup-moodle2
+ * @copyright 2010 Stephen Bourget and Jillaine Beeckman
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Structure step to restore one pcast activity
+ */
+class restore_pcast_activity_structure_step extends restore_activity_structure_step {
+
+    protected function define_structure() {
+
+        $paths = array();
+        $userinfo = $this->get_setting_value('userinfo');
+
+        $paths[] = new restore_path_element('pcast', '/activity/pcast');
+        if ($userinfo) {
+            $paths[] = new restore_path_element('pcast_episode', '/activity/pcast/episodes/episode');
+            $paths[] = new restore_path_element('pcast_view', '/activity/pcast/episodes/episode/views/view');
+        }
+
+        // Return the paths wrapped into standard activity structure
+        return $this->prepare_activity_structure($paths);
+    }
+
+    protected function process_pcast($data) {
+        global $DB;
+
+        $data = (object)$data;
+        $oldid = $data->id;
+        $data->course = $this->get_courseid();
+
+        $data->userid = $this->get_mappingid('user', $data->userid);
+        $data->assesstimestart = $this->apply_date_offset($data->assesstimestart);
+        $data->assesstimefinish = $this->apply_date_offset($data->assesstimefinish);
+        $data->timemodified = $this->apply_date_offset($data->timemodified);
+
+        // insert the pcast record
+        $newitemid = $DB->insert_record('pcast', $data);
+        // immediately after inserting "activity" record, call this
+        $this->apply_activity_instance($newitemid);
+    }
+
+    protected function process_pcast_episode($data) {
+        global $DB;
+
+        $data = (object)$data;
+        $oldid = $data->id;
+
+        $data->pcastid = $this->get_new_parentid('pcast');
+        $data->userid = $this->get_mappingid('user', $data->userid);
+        $data->timemodified = $this->apply_date_offset($data->timemodified);
+
+        $newitemid = $DB->insert_record('pcast_episodes', $data);
+        $this->set_mapping('pcast_episodes', $oldid, $newitemid);
+    }
+
+    protected function process_pcast_view($data) {
+        global $DB;
+
+        $data = (object)$data;
+        $oldid = $data->id;
+
+        $data->episodeid = $this->get_mappingid('pcast_episode', $oldid);
+        $data->userid = $this->get_mappingid('user', $data->userid);
+        //$data->lastview = $this->apply_date_offset($data->lastview);
+
+        $newitemid = $DB->insert_record('pcast_views', $data);
+        // No need to save this mapping as far as nothing depend on it
+        // (child paths, file areas nor links decoder)
+    }
+
+    protected function after_execute() {
+        // Add pcast related files, no need to match by itemname (just internally handled context)
+        $this->add_related_files('mod_pcast', 'intro', null);
+        $this->add_related_files('mod_pcast', 'logo', 'pcast');
+
+        // Add pcast related files, matching by itemname (pcast_episode)
+
+        $this->add_related_files('mod_pcast', 'episode', 'pcast_episode');
+    }
+}
