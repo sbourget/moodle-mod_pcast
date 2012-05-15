7fe745b6fa30e2971418890ae5392cbeb20e3a7a
Stephen Bourget <>
2012-05-15 14:25:26 -0400

    PCAST: Add missing mod/pcast:addinstance capability

--- db/access.php
+++ db/access.php
@@ -50,6 +50,19 @@ defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
+
+    'mod/pcast:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),    
+    
     'mod/pcast:view' => array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 2012022901;    // If version == 0 then module will not be installed
+$module->version  = 2012051500;    // If version == 0 then module will not be installed
 //$module->version  = 2010032200;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2011070101;    // Requires this Moodle version
 $module->cron     = 0;             // Period for cron to check this module (secs)
