7d3b8721ad43255e55ee6c3fa69bc7b42455434b
Stephen Bourget <>
2012-06-15 13:13:26 -0400

    PCAST: Fix broken subscribe to iTunes link

--- subscribe.php
+++ subscribe.php
@@ -37,31 +37,31 @@ require_once($CFG->libdir.'/filelib.php');
 require_once($CFG->libdir.'/rsslib.php');
 require_once($CFG->dirroot.'/mod/pcast/rsslib.php');
 
-// RSS feeds must be enabled site-wide
+// RSS feeds must be enabled site-wide.
 if (empty($CFG->enablerssfeeds)) {
     debugging('DISABLED (admin variables)');
     pcast_rss_error();
 }
 
-// RSS must be enabled for the module
+// RSS must be enabled for the module.
 if (empty($CFG->pcast_enablerssfeeds)) {
     debugging("DISABLED (module configuration)");
     pcast_rss_error();
 }
 
-// All the arguments are in the path
+// All the arguments are in the path.
 $relativepath = get_file_argument();
 if (!$relativepath) {
     pcast_rss_error();
 }
 
-// Extract relative path components into variables
+// Extract relative path components into variables.
 $args = explode('/', trim($relativepath, '/'));
 if (count($args) < 5) {
     pcast_rss_error();
 }
 
-// Validate the feed
+// Validate the feed.
 $contextid   = (int)$args[0];
 $token  = clean_param($args[1], PARAM_ALPHANUM);
 $componentname = clean_param($args[2], PARAM_FILE);
@@ -70,17 +70,17 @@ $groupid  = clean_param($args[4], PARAM_INT);
 $uservalidated = false;
 
 
-// Authenticate the user from the token
+// Authenticate the user from the token.
 $userid = rss_get_userid_from_token($token);
 if (!$userid) {
     pcast_rss_error('rsserrorauth');
 }
 
 $user = get_complete_user_data('id', $userid);
-session_set_user($user); //for login and capability checks
+session_set_user($user); //For login and capability checks.
 
 
-// Check the context actually exists
+// Check the context actually exists.
 list($context, $course, $cm) = get_context_info_array($contextid);
 
 if (!$context) {
@@ -100,14 +100,14 @@ try {
     }
 }
 
-// Now that we know that the user is vaid, lets generate see if they can see the feed
+// Now that we know that the user is vaid, lets generate see if they can see the feed.
 
-//check capabilities
+//Check capabilities.
 $cm = get_coursemodule_from_instance('pcast', $pcastid, 0, false, MUST_EXIST);
 if ($cm) {
         $modcontext = get_context_instance(CONTEXT_MODULE, $cm->id);
 
-    //context id from db should match the submitted one
+    //Context id from db should match the submitted one.
     if ($context->id==$modcontext->id && has_capability('mod/pcast:view', $modcontext)) {
         $uservalidated = true;
     }
@@ -117,21 +117,21 @@ if ($cm) {
 //Check group mode 0/1/2 (All participants)
 $groupmode = groups_get_activity_groupmode($cm);
 
-//Using Groups, check to see if user should see all participants
+//Using Groups, check to see if user should see all participants.
 if ($groupmode == SEPARATEGROUPS) {
-    //User must have the capability to see all groups or be a member of that group
+    //User must have the capability to see all groups or be a member of that group.
     $members = get_enrolled_users($context, 'mod/pcast:write', $groupid, 'u.id', 'u.id ASC');
 
-    // Is a member of the current group
+    // Is a member of the current group.
     if (!isset($members[$userid]->id) or ($members[$userid]->id != $userid)) {
 
-        // Not a member of the group, can you see all groups (from CAPS)
+        // Not a member of the group, can you see all groups (from CAPS).
         if (!has_capability('moodle/site:accessallgroups', $context, $userid)) {
             $uservalidated = false;
         }
 
     } else {
-        // Are a member of the current group
+        // Are a member of the current group.
         // Is the group #0 (Group 0 is all users)
         if ($groupid == 0 and !has_capability('moodle/site:accessallgroups', $context, $userid)) {
             $uservalidated = false;
@@ -144,12 +144,12 @@ if (!$uservalidated) {
     pcast_rss_error('rsserrorauth');
 }
 
-// OK, the use should be able to see the feed, generate the .pcast file
+// OK, the use should be able to see the feed, generate the .pcast file.
 
 $pcast = $DB->get_record('pcast', array('id' => $pcastid), '*', MUST_EXIST);
 
-// Check to se if RSS is enabled
-// NOTE: cannot use the rss_enabled_for_mod() function due to the functions internals and naming conflicts
+// Check to se if RSS is enabled.
+// NOTE: cannot use the rss_enabled_for_mod() function due to the functions internals and naming conflicts.
 if (($pcast->rssepisodes == 0)||(empty($pcast->rssepisodes))) {
     pcast_rss_error();
 }
@@ -163,11 +163,11 @@ $filename .= '_'.$groupid;
 $cachedfilepath = pcast_rss_get_file_full_name('mod_pcast', $filename);
 
 
-// Figure out the URL for the podcast based on the user info
-$args = $pcast->id . '/'.$userid.'/'.$groupid;
+// Figure out the URL for the podcast based on the user info,
+$args = $pcast->id . '/' .$groupid;
 $url = new moodle_url(rss_get_url($context->id, $userid, 'pcast', $args));
 
-// Build the .pcast file
+// Build the .pcast file.
 $rss = pcast_build_pcast_file($pcast, $url);
 
 //Save the XML contents to file.
@@ -178,19 +178,19 @@ if (!$status) {
 }
 
 
-// Check that file exists
+// Check that file exists.
 if (empty($cachedfilepath) || !file_exists($cachedfilepath)) {
     die($cachedfilepath);
     pcast_rss_error();
 }
 
 // Send the .pcast file to the user!
-send_file($cachedfilepath, 'rss.pcast', 0, 0, 0, 1);   // DO NOT CACHE
+send_file($cachedfilepath, 'rss.pcast', 0, 0, 0, 1);   // DO NOT CACHE.
 
 
 
 /*
- * Sends an error formatted as an rss file and then dies
+ * Sends an error formatted as an rss file and then dies.
  */
 function pcast_rss_error($error='rsserror', $filename='rss.xml', $lifetime=0) {
     send_file(rss_geterrorxmlfile($error), $filename, $lifetime, false, true);
