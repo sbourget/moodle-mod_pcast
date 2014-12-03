3cf2737d9d6656d24dee432b01d6b08c385b505a
Stephen Bourget <sbourget@goffstown.k12.nh.us>
2014-12-03 18:51:23 -0500

    Pcast: Remove FEATURE_GROUPMEMBERSONLY
    Remove old FEATURE_GROUPMEMBERSONLY as no longer required.

--- lib.php
+++ lib.php
@@ -77,7 +77,6 @@ function pcast_supports($feature) {
     switch($feature) {
         case FEATURE_GROUPS:                  return true;
         case FEATURE_GROUPINGS:               return true;
-        case FEATURE_GROUPMEMBERSONLY:        return true;
         case FEATURE_MOD_INTRO:               return true;
         case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
         case FEATURE_GRADE_HAS_GRADE:         return true;
