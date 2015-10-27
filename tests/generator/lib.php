f9eba4ba675599403b255333648d3d11c2b5cd8a
Stephen Bourget <sbourget@goffstown.k12.nh.us>
2015-10-26 22:17:00 -0400

    Pcast: Add generator and unit tests.

---
+++ tests/generator/lib.php
@@ -0,0 +1,122 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * mod_pcast data generator.
+ *
+ * @package    mod_pcast
+ * @category   test
+ * @copyright  2015 Stephen Bourget
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * mod_pcast data generator class.
+ *
+ * @package    mod_pcast
+ * @category   test
+ * @copyright  2015 Stephen Bourget
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_pcast_generator extends testing_module_generator {
+
+    /**
+     * @var int keep track of how many episodes have been created.
+     */
+    protected $episodecount = 0;
+
+    /**
+     * To be called from data reset code only,
+     * do not use in tests.
+     * @return void
+     */
+    public function reset() {
+        $this->episodecount = 0;
+        parent::reset();
+    }
+
+    public function create_instance($record = null, array $options = null) {
+        global $CFG;
+
+        // Add default values for pcast.
+        $record = (array)$record + array(
+            'userscancomment' => 0,
+            'userscancategorize' => 0,
+            'userscanpost' => 1,
+            'maxbytes' => $CFG->maxbytes,
+            'episodesperpage' => 10,
+            'requireapproval' => 1,
+            'displayauthor' => 0,
+            'displayviews' => 0,
+            'image' => 0,
+            'imageheight' => 144,
+            'imagewidth' => 144,
+            'rssepisodes' => 0,
+            'rssorder' => 0,
+            'enablerssfeed' => 0,
+            'enablerssitunes' => 0,
+            'explicit' => 0,
+            'topcategory' => 0,
+            'nestedcategory' => 0,
+            'scale' => 100,
+            'assessed' => 0,
+        );
+
+        return parent::create_instance($record, (array)$options);
+    }
+
+    public function create_content($pcast, $record = array()) {
+        global $DB, $USER, $CFG;
+        $this->episodecount++;
+        $now = time();
+        $record = (array)$record + array(
+            'pcastid' => $pcast->id,
+            'timecreated' => $now,
+            'timemodified' => $now,
+            'userid' => $USER->id,
+            'name' => 'Episode '.$this->episodecount,
+            'summary' => 'Description of pcast entry '.$this->episodecount,
+            'summaryformat' => FORMAT_MOODLE
+        );
+        // Media File
+        
+        
+        // Episode approval
+        if (!isset($record['approved'])) {
+            if($pcast->requireapproval) {
+                $context = context_module::instance($pcast->cmid);
+                if(has_capability('mod/pcast:approve', $context)) {
+                    // User can approve episodes so automatically approve theirs
+                    $record['approved'] = 1;
+                } else {
+                    // Episode needs approval.
+                    $record['approved'] = 0;
+                }
+                
+            } else {
+                // No approval required.
+                $record['approved'] = 1;
+            }
+            
+        }
+
+        $id = $DB->insert_record('pcast_episodes', $record);
+
+        return $DB->get_record('pcast_episodes', array('id' => $id), '*', MUST_EXIST);
+    }
+}
---
+++ tests/generator_test.php
@@ -0,0 +1,74 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * mod_pcast generator tests
+ *
+ * @package    mod_pcast
+ * @category   test
+ * @copyright  2015 Stephen Bourget
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Genarator tests class for mod_pcast.
+ *
+ * @package    mod_pcast
+ * @category   test
+ * @copyright  2015 Stephen Bourget
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_pcast_generator_testcase extends advanced_testcase {
+
+    public function test_create_instance() {
+        global $DB;
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $course = $this->getDataGenerator()->create_course();
+
+        $this->assertFalse($DB->record_exists('pcast', array('course' => $course->id)));
+        $pcast = $this->getDataGenerator()->create_module('pcast', array('course' => $course));
+        $records = $DB->get_records('pcast', array('course' => $course->id), 'id');
+        $this->assertCount(1, $records);
+        $this->assertTrue(array_key_exists($pcast->id, $records));
+
+        $params = array('course' => $course->id, 'name' => 'Another pcast');
+        $pcast = $this->getDataGenerator()->create_module('pcast', $params);
+        $records = $DB->get_records('pcast', array('course' => $course->id), 'id');
+        $this->assertCount(2, $records);
+        $this->assertEquals('Another pcast', $records[$pcast->id]->name);
+    }
+
+    public function test_create_content() {
+        global $DB;
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $course = $this->getDataGenerator()->create_course();
+        $pcast = $this->getDataGenerator()->create_module('pcast', array('course' => $course));
+        /** @var mod_pcast_generator $pcastgenerator */
+        $pcastgenerator = $this->getDataGenerator()->get_plugin_generator('mod_pcast');
+
+        $episode1 = $pcastgenerator->create_content($pcast);
+        $episode2 = $pcastgenerator->create_content($pcast, array('name' => 'Custom episode'));
+        $records = $DB->get_records('pcast_episodes', array('pcastid' => $pcast->id), 'id');
+        $this->assertCount(2, $records);
+        $this->assertEquals($episode1->id, $records[$episode1->id]->id);
+        $this->assertEquals($episode2->id, $records[$episode2->id]->id);
+        $this->assertEquals('Custom episode', $records[$episode2->id]->name);
+    }
+}
