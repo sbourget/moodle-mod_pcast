cedd957d89a9bc5dd87314261d61fea12aa93f5d
Stephen Bourget <steve.bourget@sau19.org>
2016-03-15 12:47:17 -0400

    Pcast: Apply fixes from MDL-53039

--- subscribe.php
+++ subscribe.php
@@ -91,7 +91,7 @@ try {
     $autologinguest = true;
     $setwantsurltome = true;
     $preventredirect = true;
-    require_login($course, $autologinguest, $cm, $setwantsurltome, $preventredirect);
+    require_course_login($course, $autologinguest, $cm, $setwantsurltome, $preventredirect);
 } catch (Exception $e) {
     if (isguestuser()) {
         pcast_rss_error('rsserrorguest');
