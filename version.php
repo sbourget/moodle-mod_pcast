57527e86ef0c0c44d109d817db565eae9db5320f
Stephen Bourget <steve.bourget@sau19.org>
2017-11-08 14:49:25 -0500

    Pcast: Version bump for release.

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017042100;    // If version == 0 then module will not be installed.
+$plugin->version  = 2017042101;    // If version == 0 then module will not be installed.
 $plugin->requires = 2017042100;    // Requires this Moodle version.
 $plugin->cron     = 0;             // Period for cron to check this module (secs).
 $plugin->component = 'mod_pcast';  // Full name of the plugin (used for diagnostics).
