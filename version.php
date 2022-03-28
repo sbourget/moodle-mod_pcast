ce0813cdb50871899203a7795c154aad3680bc05
sbourget <steve.bourget@sau19.org>
2022-03-27 20:38:39 -0400

    mod_pcast: Bump version for 4.0

--- version.php
+++ version.php
@@ -28,10 +28,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021112800;    // If version == 0 then module will not be installed.
-$plugin->requires = 2021112600;    // Requires this Moodle version.
+$plugin->version  = 2022030300;    // If version == 0 then module will not be installed.
+$plugin->requires = 2022030300;    // Requires Moodle version 4.0 or later.
 $plugin->cron     = 0;             // Period for cron to check this module (secs).
 $plugin->component = 'mod_pcast';  // Full name of the plugin (used for diagnostics).
 
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "4.0 (2021112800)";  // User-friendly version number.
+$plugin->release = "4.0 (2022030300)";  // User-friendly version number.
