8e25eac9a57ee2663e028664a235b2a753aa6bcc
Stephen Bourget <Stephen.Bourget@sau19.org>
2022-11-22 16:20:19 -0500

    mod_pcast: fix required version to 4.1rc-1

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2022112200;    // If version == 0 then module will not be installed.
-$plugin->requires = 2022112200;    // Requires Moodle version 4.0 or later.
+$plugin->requires = 2022111800;    // Requires Moodle version 4.0 or later.
 $plugin->cron     = 0;             // Period for cron to check this module (secs).
 $plugin->component = 'mod_pcast';  // Full name of the plugin (used for diagnostics).
 
